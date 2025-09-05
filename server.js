b711a0c17365a32339b7e9a5d41a141fae807381
DaUmega <daumega@gmail.com>
2025-09-05 10:35:28 -0600

    reversing port number

--- server.js
+++ server.js
@@ -146,7 +146,7 @@ io.on("connection", (socket) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
 	console.log(`Server running on http://localhost:${PORT}`);
 });
